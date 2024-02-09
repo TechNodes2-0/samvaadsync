635f79917005431482e5df581c8685b9dd5a7fd2
Vinayak Vispute <vinayakvispute4@gmail.com>
2024-02-09 21:46:13 +0530

    Feat: ChatMessage Components Done

--- client/app/(root)/chat/components/ReceivedMessage.jsx
+++ client/app/(root)/chat/components/ReceivedMessage.jsx
@@ -1,14 +1,25 @@
 import React from "react";
 
-const ReceivedMessage = () => {
+const ReceivedMessage = ({
+  messageContent = "Hey How are you today?",
+  index = 1,
+  timestamp = new Date().toLocaleTimeString(),
+  username = "Anonymous",
+}) => {
+  const firstLetter = username.charAt(0).toUpperCase();
   return (
     <div className="col-start-6 col-end-13 p-3 rounded-lg">
       <div className="flex items-center justify-start flex-row-reverse">
         <div className="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-500 flex-shrink-0">
-          A
+          {firstLetter}
         </div>
         <div className="relative mr-3 text-sm bg-indigo-100 py-2 px-4 shadow rounded-xl">
-          <div>I'm ok what about you?</div>
+          <div>{messageContent}</div>
+        </div>
+        <div className="flex items-center justify-between mt-2 text-xs text-gray-500">
+          <span>{username}</span>
+          {/* <span>{formatTimestamp(timestamp)}</span> */}
+          <span>{timestamp}</span>
         </div>
       </div>
     </div>
--- client/app/(root)/chat/components/YourMessage.jsx
+++ client/app/(root)/chat/components/YourMessage.jsx
@@ -1,14 +1,26 @@
 import React from "react";
 
-const YourMessage = () => {
+const YourMessage = ({
+  messageContent = "Hey How are you today?",
+  index = 1,
+  timestamp = new Date().toLocaleTimeString(),
+  username = "Anonymous",
+}) => {
+  const firstLetter = username.charAt(0).toUpperCase();
+
   return (
     <div className="col-start-1 col-end-8 p-3 rounded-lg">
       <div className="flex flex-row items-center">
         <div className="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-500 flex-shrink-0">
-          A
+          {firstLetter}
         </div>
         <div className="relative ml-3 text-sm bg-white py-2 px-4 shadow rounded-xl">
-          <div>Hey How are you today?</div>
+          <div>{messageContent}</div>
+        </div>
+        <div className="flex items-center justify-between mt-2 text-xs text-gray-500">
+          <span>{username}</span>
+          {/* <span>{formatTimestamp(timestamp)}</span> */}
+          <span>{timestamp}</span>
         </div>
       </div>
     </div>
