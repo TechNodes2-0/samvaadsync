12297122190ac5e1b4f43d1180a78f88d19282b1
yash suthar <ysuthar8055@gmail.com>
2024-02-10 10:07:50 +0530

    text updated

--- client/components/shared/Cards.jsx
+++ client/components/shared/Cards.jsx
@@ -5,16 +5,16 @@ const CardBlog = () => {
     <section className="text-gray-400 dark:bg-gray-800 body-font">
       <div className="container flex flex-wrap px-5 py-24 mx-auto">
         <div className="flex flex-col w-full mb-20 text-center">
-          <h2 className="mb-1 text-xs font-medium tracking-widest dark:text-blue-400 title-font">
-            ROOF PARTY POLAROID
+          <h2 className="mb-1 text-xs font-medium tracking-widest dark:text-blue-600 title-font">
+            What it offers
           </h2>
-          <h1 className="text-2xl font-medium sm:text-3xl title-font dark:text-white">
-            Master Cleanse Reliac Heirloom
+          <h1 className="text-2xl font-medium sm:text-3xl title-font text-blue-800 ">
+            About
           </h1>
         </div>
         <div className="flex flex-wrap -m-4">
           <div className="p-4 md:w-1/3">
-            <div className="flex flex-col h-full p-8 bg-gray-800 rounded-lg bg-opacity-60">
+            <div className="flex flex-col h-full p-8 bg-gray-200 shadow-lg rounded-lg bg-opacity-60">
               <div className="flex items-center mb-3">
                 <div className="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 mr-3 text-white bg-blue-500 rounded-full">
                   <svg
@@ -29,8 +29,8 @@ const CardBlog = () => {
                     <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
                   </svg>
                 </div>
-                <h2 className="text-lg font-medium text-white title-font">
-                  Shooting Stars
+                <h2 className="text-lg font-medium text-black  title-font">
+                  Live chat translation and easy retrieval
                 </h2>
               </div>
               <div className="flex-grow">
@@ -56,7 +56,7 @@ const CardBlog = () => {
             </div>
           </div>
           <div className="p-4 md:w-1/3">
-            <div className="flex flex-col h-full p-8 bg-gray-800 rounded-lg bg-opacity-60">
+            <div className="flex flex-col h-full p-8 bg-gray-200 shadow-lg rounded-lg bg-opacity-60">
               <div className="flex items-center mb-3">
                 <div className="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 mr-3 text-white bg-blue-500 rounded-full">
                   <svg
@@ -72,8 +72,8 @@ const CardBlog = () => {
                     <circle cx="12" cy="7" r="4"></circle>
                   </svg>
                 </div>
-                <h2 className="text-lg font-medium text-white title-font">
-                  The Catalyzer
+                <h2 className="text-lg font-medium text-black  title-font">
+                  Video summary and after analysis
                 </h2>
               </div>
               <div className="flex-grow">
@@ -99,7 +99,7 @@ const CardBlog = () => {
             </div>
           </div>
           <div className="p-4 md:w-1/3">
-            <div className="flex flex-col h-full p-8 bg-gray-800 rounded-lg bg-opacity-60">
+            <div className="flex flex-col h-full p-8 bg-gray-200 shadow-lg rounded-lg bg-opacity-60">
               <div className="flex items-center mb-3">
                 <div className="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 mr-3 text-white bg-blue-500 rounded-full">
                   <svg
@@ -116,8 +116,8 @@ const CardBlog = () => {
                     <path d="M20 4L8.12 15.88M14.47 14.48L20 20M8.12 8.12L12 12"></path>
                   </svg>
                 </div>
-                <h2 className="text-lg font-medium text-white title-font">
-                  Neptune
+                <h2 className="text-lg font-medium text-black  title-font">
+                  Integration for application tracking system for data insights
                 </h2>
               </div>
               <div className="flex-grow">
