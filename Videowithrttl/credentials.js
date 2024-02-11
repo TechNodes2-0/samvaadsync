8f03f5cc6d49af51e346b2eebd8e46d521b9540a
Nishitbariya <85815172+Nishitbaria@users.noreply.github.com>
2024-02-11 10:47:53 +0530

    fix

--- Videowithrttl/credentials.js
+++ Videowithrttl/credentials.js
@@ -1,14 +1,8 @@
 module.exports = {
-  // You can find the API key and API secret in the dashboard: https://tokbox.com/account/
   VONAGE_API_KEY: "47852881",
   VONAGE_API_SECRET: "ef4a56b021dab421d337f97a69b42b08babe08a3",
-
-  // The URL of the running application without "https://" and trailing "/" (ex. abc123.com). This must be a publically available URL on the internet.
   APP_DOMAIN: "video-server-788j.onrender.com",
-
-  // Replace with your own subscription key, service region (e.g., "westus"),
-  // and recognition language. Can be found in the dashboard: https://portal.azure.com/
   AZURE_SUBSCRIPTION_KEY: "0dcf03ad184348bfaf7c793231f98267",
-  AZURE_SERVICE_REGION: "eastus", // e.g., "westus"
-  DEFAULT_TARGET_LANGUAGE: "hi", // view full list here: https://learn.microsoft.com/en-us/azure/ai-services/speech-service/language-support?tabs=speech-translation
+  AZURE_SERVICE_REGION: "eastus",
+  DEFAULT_TARGET_LANGUAGE: "hi",
 };
