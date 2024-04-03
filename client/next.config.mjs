aba7c0e8be2413ffa187f5edae3853059010518e
Nishitbariya <85815172+Nishitbaria@users.noreply.github.com>
2024-04-03 13:54:32 +0530

    Update next.config.mjs to remove remotePatterns

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,17 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "*",
-      },
-      {
-        protocol: "http",
-        hostname: "*",
-      },
-    ],
-  },
-};
+const nextConfig = {};
 
-module.exports = nextConfig;
+export default nextConfig;
