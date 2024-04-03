55414e725b6a7aeb194e46449e3ab89d76947045
Nishitbariya <85815172+Nishitbaria@users.noreply.github.com>
2024-04-03 13:53:37 +0530

    Remove experimental configuration and update images remotePatterns

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    mdxRs: true,
-    serverComponentsExternalPackages: ["mongoose"],
-  },
   images: {
     remotePatterns: [
       {
